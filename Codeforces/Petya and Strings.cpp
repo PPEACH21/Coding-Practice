35b2c40321eac1b4f84e7e3fda19493bd9ed0bab
SoHizz21 <sohizz12012gmail.com>
2024-09-17 23:36:20 +0700

    Petya and Strings

---
+++ Codeforces/Petya and Strings.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+int main(){
+    string s,s1;
+    cin >> s >> s1;
+
+    for(int i=0;i<s.length();i++){
+        s1[i]=tolower(s1[i]);
+    }
+
+    if(s > s1){
+        cout << "1";
+    }else if(s1 > s){
+        cout << "-1";
+    }else{
+        cout << "0";
+    }
+}
