7db457f2fc252385b4a23f5bcdfacb4c0943e5e7
SoHizz21 <sohizz1201@gmail.com>
2024-09-23 18:40:23 +0700

    800 sum.cpp

---
+++ Codeforces/Sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n; 
+    cin >> n;
+    
+    for(int i=0;i<n;i++){
+        int a,b,c;
+        cin >> a>> b>>c;
+
+        if(a+b == c){
+            cout << "YES" << endl;
+        }else if(a+c == b){
+            cout << "YES" << endl;
+        }else if(b+c == a){
+            cout << "YES" << endl;
+        }else{
+            cout << "NO" <<endl;
+        }
+    }
+}
