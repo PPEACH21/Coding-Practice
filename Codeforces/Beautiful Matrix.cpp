d46930a247e3a9db3c85a47ceea1370f2b13574f
SoHizz21 <sohizz1201@gmail.com>
2024-09-25 22:19:30 +0700

    800 Beautiful Matrix.cpp

--- Codeforces/Beautiful Matrix.cpp
+++ Codeforces/Beautiful Matrix.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-#include <algorithm>
+#include <vector>
 using namespace std;
 
 int main(){
-	int x,y,c;
+	int x,y;
 	for(int i=0;i<5;i++){
 		for(int j=0;j<5;j++){
-			cin >> c;
-			if(map[i][j]==1){
+			int n;
+			cin >> n;
+			if(n==1){
 				x=j;
 				y=i;
 			}
 		}
 	}
-	
-	cout << ans;
-	for(int i=0;i<5;i++){
-		for(int j=0;j<5;j++){
-			cout << map[i][j] <<  " ";
-		}
-		cout << endl;
+
+	int x1=0,y1=0;
+	if(x>2){
+		x1=x-2;
+	}else if(x<2){
+		x1=2-x;
 	}
+
+	if(y>2){
+		y1=y-2;
+	}else if(y<2){
+		y1=2-y;
+	}
+
+
+	cout << x1 +y1;
 }
