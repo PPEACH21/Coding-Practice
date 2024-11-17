2ce5cdb2b990edde5cbc3b004c6b6af53eff99c1
SoHizz21 <sohizz1201@gmail.com>
2024-11-17 18:07:42 +0700

    Sakurako and Kosuke 800

---
+++ Codeforces/Sakurako and Kosuke.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int c;
+        cin >> c;
+        if(c%2 == 1){
+            cout << "Kosuke" << endl;
+        }else{
+            cout << "Sakurako" << endl;
+        }
+    }
+}
